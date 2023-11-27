949cb036f8dce899609f7eb5cb4d3c6cdb88f745
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-11-27 13:49:46 +0200

    SPIR-13: add api key and base url to build config
    - rename app package

--- app/src/main/java/com/example/spiritsnack/MainActivity.kt
+++ app/src/main/java/com/example/spiritsnack/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.spiritsnack.ui.theme.ComoseNavigationTheme
+import com.example.spiritsnack.ui.theme.ComposeNavigationTheme
 
 class MainActivity : ComponentActivity() {
 
@@ -18,7 +18,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComoseNavigationTheme {
+            ComposeNavigationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -42,7 +42,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ComoseNavigationTheme {
+    ComposeNavigationTheme {
         Greeting("Android")
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,6 +13,6 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ComoseNavigation"
+rootProject.name = "ComposeNavigation"
 include(":app")
  
